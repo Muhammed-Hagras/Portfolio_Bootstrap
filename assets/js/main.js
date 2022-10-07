9ba71a1c960814cfb32bf455095376748faafdc0
Muhammed-Hagras <40708938+Muhammed-Hagras@users.noreply.github.com>
2022-10-07 20:18:33 +0200

    Geolocation problem solved

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,3 +96,34 @@ const swipercont = new Swiper('#swipercon .swiper', {
   
   
 
+  /*
+  Google Maps 
+  */
+
+
+
+  function getlocation(){ 
+    if(navigator.geolocation){ 
+        navigator.geolocation.getCurrentPosition(showPos); 
+    }
+    else{
+        alert("Sorry! your Browser does not support Geolocation API")
+    }
+}
+
+  function showPos(position){ 
+    latt = position.coords.latitude; 
+    long = position.coords.longitude; 
+    var lattlong = new google.maps.LatLng(latt, long); 
+    var myOptions = { 
+        center: lattlong, 
+        zoom: 15, 
+        mapTypeControl: true, 
+        navigationControlOptions: {style:google.maps.NavigationControlStyle.SMALL} 
+    } 
+    var maps = new google.maps.Map(document.getElementById("map"), myOptions); 
+    var markers = 
+    new google.maps.Marker({position:lattlong, map:maps, title:"You are here!"}); 
+} 
+
+getlocation();
